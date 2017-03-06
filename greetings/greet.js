c8bf449b466b65e9085a25a27dd6cecbe423b251
Nonzulu <knzulu16@gmail.com>
2017-03-06 17:41:56 +0200

    latest

---
+++ greetings/greet.js
@@ -0,0 +1,21 @@
+
+
+function myGreet(){
+
+  var greeting = document.getElementById('box')
+  // .addEventListener("click",function(){
+
+    document.getElementById('para').innerHTML = 'Hello ' + greeting.value;
+  };
+
+
+if(document.getElementById('Isi'.checked)){
+  document.getElementById('para').innerHTML='Molo'
+}
+else if (Edocument.getElementById('Eng'.checked)){
+  document.getElementById('para').innerHTML='Hello'
+}
+else if(document.getElementById('Afri'.checked)){
+  document.getElementById('para').innerHTML='Goeie dag'
+}
+  // var greet = document.getElementById('myBtn').myTextBox= 'hello';
